e467c0f740aef59f744e81f0b9ea16d71a8f1e9b
jethington <jethington4@gmail.com>
2017-08-15 22:59:23 -0400

    Partially implemented parsing input file
    Still need to implement label handling, write tests

--- leds.rs
+++ leds.rs
@@ -23,6 +23,13 @@ use std::io::prelude::*;
                       djnz <labelref>             
 */
 
+enum Line {
+    Instruction( Instruction ),
+    Label( String ),
+    Empty,
+    ParseError
+}
+
 enum Instruction {
     LoadA( u8 ),
     LoadB( u8 ),
@@ -36,7 +43,6 @@ enum Instruction {
 impl Instruction {
     fn to_string(&self) -> String {
         match self {
-            //LoadA( ref a) => (),
             &Instruction::LoadA( a ) => format!("ld a,{}", a),
             &Instruction::LoadB( b ) => format!("ld b,{}", b),
             &Instruction::Out => String::from("out (0),a"),
@@ -62,16 +68,71 @@ fn leds_to_string(register_a: u8) -> String {
 #[test]
 #[allow(dead_code)]
 fn test_leds_to_string() {
-    assert!(leds_to_string(0x80) == "*.......");
+    assert!(leds_to_string(0x80) == "*......."); // TODO assert_eq!() is better ??
     assert!(leds_to_string(0x55) == ".*.*.*.*");
     assert!(leds_to_string(0xAA) == "*.*.*.*.");
     assert!(leds_to_string(0x0F) == "....****");
+}
+
+
+
+fn parse(line: &str, labels: &Vec<String>) -> Line {
+    // note: allowing the following even though they are technically not part of the problem description:
+    //  line -> <whitespace> <instruction> <whitespace>
+    //  line -> <whitespace> <label> <whitespace>
+
+    let trimmed: &str = line.trim();
+    match trimmed {
+        "rlca" => Line::Instruction(Instruction::Rlca),
+        "rrca" => Line::Instruction(Instruction::Rrca),
+        "out (0),a" => Line::Instruction(Instruction::Out),
+        "" => Line::Empty,
+        _ => {
+            if trimmed.len() < 5 {
+                try_label(trimmed)
+            }
+            else {
+                match &trimmed[0..5] {
+                    "ld a," => try_register_load(trimmed, true),
+                    "ld b," => try_register_load(trimmed, false),
+                    "djnz " => try_jump(trimmed, labels),
+                    _ => try_label(trimmed),
+                }
+            }
+        },
+    }
+}
+
+fn try_jump(trimmed: &str, labels: &Vec<String>) -> Line {
+    Line::ParseError
+}
+
+fn try_label(trimmed: &str) -> Line {
+    // only alphanumeric chars and ends in a :
+    Line::ParseError
+}
+
+// TODO: boolean instead of another enum is kind of a hack, but im lazy
+fn try_register_load(trimmed: &str, register_a: bool) -> Line {
+    let load_value = &trimmed[5..trimmed.len()].parse::<u8>();
+    match (register_a, load_value) {
+        (true, &Ok(a)) => Line::Instruction(Instruction::LoadA(a)),
+        (false, &Ok(b)) => Line::Instruction(Instruction::LoadB(b)),
+        (_, &Err(_)) => Line::ParseError,
+    }
 }
 
 fn run_file(file_name: &str) {
     let mut file = File::open(file_name).expect(&(format!("Failed to open {}", file_name))); // TODO: error handling here!
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect(&(format!("Failed to read from {}", file_name)));
+    let mut labels = vec![];    
+    for line in contents.lines() {
+        match parse(line, &labels) {
+            Line::Label(name) => labels.push(String::from(name)),
+            _ => ()
+        }
+    }
 }
 
 fn main() {
