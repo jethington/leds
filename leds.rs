e8f65242f2d1214f98729eb39198f3946b6e4d1c
jethington <jethington4@gmail.com>
2017-08-17 22:58:00 -0400

    Added several tests, still working on parsing input files

--- leds.rs
+++ leds.rs
@@ -6,7 +6,7 @@
 //use std::io::Write;
 use std::fs::File;
 use std::io::prelude::*;
-
+use std::collections::HashMap;
 
 
 /* grammar of our mini programming language:
@@ -65,18 +65,7 @@ fn leds_to_string(register_a: u8) -> String {
     result
 }
 
-#[test]
-#[allow(dead_code)]
-fn test_leds_to_string() {
-    assert!(leds_to_string(0x80) == "*......."); // TODO assert_eq!() is better ??
-    assert!(leds_to_string(0x55) == ".*.*.*.*");
-    assert!(leds_to_string(0xAA) == "*.*.*.*.");
-    assert!(leds_to_string(0x0F) == "....****");
-}
-
-
-
-fn parse(line: &str, labels: &Vec<String>) -> Line {
+fn parse(line: &str, labels: &HashMap<String, usize>) -> Line {
     // note: allowing the following even though they are technically not part of the problem description:
     //  line -> <whitespace> <instruction> <whitespace>
     //  line -> <whitespace> <label> <whitespace>
@@ -89,27 +78,39 @@ fn parse(line: &str, labels: &Vec<String>) -> Line {
         "" => Line::Empty,
         _ => {
             if trimmed.len() < 5 {
-                try_label(trimmed)
+                try_label(trimmed, labels)
             }
             else {
                 match &trimmed[0..5] {
                     "ld a," => try_register_load(trimmed, true),
                     "ld b," => try_register_load(trimmed, false),
                     "djnz " => try_jump(trimmed, labels),
-                    _ => try_label(trimmed),
+                    _ => try_label(trimmed, labels),
                 }
             }
         },
     }
 }
 
-fn try_jump(trimmed: &str, labels: &Vec<String>) -> Line {
-    Line::ParseError
+fn try_jump(trimmed: &str, labels: &HashMap<String, usize>) -> Line {
+    //Line::ParseError
+    
+    // note: am I in trouble because trimmed is an &str instead of a string?
+    
+    match labels.get(trimmed.to_owned()) {
+        Some(line) => Line::Instruction(Instruction::Djnz{ line }),
+        None => Line::ParseError,
+    }
 }
 
-fn try_label(trimmed: &str) -> Line {
-    // only alphanumeric chars and ends in a :
-    Line::ParseError
+fn try_label(trimmed: &str, labels: &HashMap<String, usize>) -> Line {
+    if labels.contains_key(trimmed) {
+        Line::ParseError
+    }
+    else {
+        // TODO: still need to see if it makes a valid label
+        Line::ParseError
+    } 
 }
 
 // TODO: boolean instead of another enum is kind of a hack, but im lazy
@@ -126,12 +127,27 @@ fn run_file(file_name: &str) {
     let mut file = File::open(file_name).expect(&(format!("Failed to open {}", file_name))); // TODO: error handling here!
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect(&(format!("Failed to read from {}", file_name)));
-    let mut labels = vec![];    
+    //let mut labels = vec![]; // TODO: replace with map
+    
+    // JCE: does &str actually work here?  looks like possibly yes:
+    // https://users.rust-lang.org/t/efficient-string-hashmaps-for-a-frequency-count/7752/2
+    // would need to refactor, label is technically a slice of line but currently a copy is created
+    
+    let mut instructions: Vec<Instruction> = vec![];
+    let mut labels: HashMap<String, usize> = HashMap::new(); // TODO: is usize the right type of index?
+    let mut index: usize = 0;
     for line in contents.lines() {
         match parse(line, &labels) {
-            Line::Label(name) => labels.push(String::from(name)),
+            Line::Label(name) => labels.insert(String::from(name), index),
             _ => ()
         }
+        index += 1;
+    }
+    
+    index = 0;
+    while (index < instructions.len()) {
+    
+        index += 1;
     }
 }
 
@@ -139,4 +155,57 @@ fn main() {
     print!("Hello!");
 
     run_file("input1.txt");
+}
+
+#[test]
+#[allow(dead_code)]
+fn test_try_register_load() {
+    assert_eq!(try_register_load("ld a,4", true), Line::Instruction(Instruction::LoadA(4)));
+    assert_eq!(try_register_load("ld a, 4", true), Line::Instruction(Instruction::LoadA(4)));
+    assert_eq!(try_register_load("ld b, 4", false"), Line::Instruction(Instruction::LoadB(4)));
+    assert_eq!(try_register_load("ld a, 400", true), Line::ParseError);
+    assert_eq!(try_register_load("ld a, a123", true), Line::ParseError);
+    assert_eq!(try_register_load("ld a,", true), Line::ParseError);
+}
+
+#[test]
+#[allow(dead_code)]
+fn test_try_label {
+    assert_eq!(test_try_label("lbl:"), Line::Label("lbl")); // String::from() ?
+    assert_eq!(test_try_label("longer_label:"), Line::Label("longer_label"));
+    assert_eq!(test_try_label("bad:label:"), Line::ParseError);
+    assert_eq!(test_try_label("toomanycolons::"), Line::ParseError);
+    assert_eq!(test_try_label("foo"), Line::ParseError);
+    assert_eq!(test_try_label(":bar"), Line::ParseError);
+}
+
+#[test]
+#[allow(dead_code)]
+fn test_try_jump() {
+    
+}
+
+#[test]
+#[allow(dead_code)]
+fn test_parse() {
+    // make sure correct boolean is supplied to try_register_load()
+    assert_eq!(parse("  ld a,4"), Line::Instruction(Instruction::LoadA(4)));
+    assert_eq!(parse("ld b, 4   "), Line::Instruction(Instruction::LoadB(4)));
+    
+    // test instructions that don't have a dedicated function
+    assert_eq!(parse("    "), Line::Empty;
+    assert_eq!(parse(" rlca  "), Line::Instruction(Instruction::Rlca));
+    assert_eq!(parse("  rrca "), Line::Instruction(Instruction::Rrca));
+    assert_eq!(parse(" out (0),a "), Line::Instruction(Instruction::Out));
+    
+    // note: whitespace handling is covered in above tests
+}
+
+#[test]
+#[allow(dead_code)]
+fn test_leds_to_string() {
+    assert_eq!(leds_to_string(0x80), "*.......");
+    assert_eq!(leds_to_string(0x55), ".*.*.*.*");
+    assert_eq!(leds_to_string(0xAA), "*.*.*.*.");
+    assert_eq!(leds_to_string(0x0F), "....****");
 }
